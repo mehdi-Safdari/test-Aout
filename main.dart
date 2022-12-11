e2d8fc1618f6336587ac045f20daa7290ab409f1
Mehdi Safdari <mti.safdari@gmail.com>
2022-12-11 21:33:14 +0330

    for test

---
+++ main.dart
@@ -0,0 +1,15 @@
+import 'dart:async';
+
+void main(List<String> args) {
+  Future<int>.delayed(
+    Duration(seconds: 5),
+    () => throw 'error!',
+  )
+      .then((value) => print(value))
+      .catchError(
+        (e) => print('object: $e'),
+        test: (e) => e.runtimeType == String,
+      )
+      .whenComplete(() => print('all finished'));
+  print('LOADING...');
+}
